9b1fdfa2802d05b2b1656c908a65084a93112189
项工 <xianggong@anve.com>
2019-07-17 09:12:42 +0800

    init commit

---
+++ App/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by 項普華 on 2019/7/17.
+//  Copyright © 2019 項普華. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '9.0'
+
+target 'App' do
+  use_frameworks!
+  pod 'Alamofire' #网络请求框架
+  pod 'SnapKit' #布局框架
+  pod 'ReactiveSwift' #响应式编程
+
+  target 'AppTests' do
+    inherit! :search_paths
+  end
+
+  target 'AppUITests' do
+    inherit! :search_paths
+  end
+
+end
